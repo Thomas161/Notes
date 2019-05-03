45ae0c9558b550c145fb131687d766988319693a
Tom <Thomas.Magin@Cognizant.com>
2019-05-03 13:20:24 +1000

    adding react errors

--- react.js
+++ react.js
@@ -103,3 +103,180 @@ const a = Object.assign({});
 /*
 setState is asynchronous
 */
+
+state = {
+    counter: 1,
+};
+
+incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + 1 });
+};
+//expected value to be 4, but answer is 2, not enough time to change the value when next call is done
+
+/*
+Forgot Arrows in Callback
+*/
+
+state = {
+    counter: 1,
+};
+
+incrementCounter = (amount) => {
+    this.setState({ counter: this.state.counter + amount });
+};
+
+render() {
+    return (
+        <div>
+            <p>{this.state.counter}</p>
+            <button onClick={this.incrementCounter(5)} />
+        </div>
+    );
+}
+//The correct way to assign a function at events is to use arrow function
+onClick = {() => this.incrementCounter(5)}
+//When you don’t need parameter in your function or parameters are given directly by the event
+onClick = { this.myFunction }
+// Is the same as onClick={(e) => this.myFunction(e)}
+
+/*
+Objects are not valid as a React child”
+*/
+import * as React from "react";
+type Status =
+    | "Open"
+    | "Closed"
+
+type PureProps = {
+    status: Status;
+};
+
+function StatusDescription(status: Status): JSX.Element {
+    return <span>{status}</span>;;
+}
+
+export class StatusSummary extends React.PureComponent {
+    render() {
+        return (
+            <StatusDescription status={this.props.status} />
+        );
+    }
+}
+
+//fix => return <span>{status}</span> has two properties Open or closed, 
+//solution status.Open or status.Closed
+
+
+/*
+Currying
+Currying is transforming a function with N number of parameters into a chain of N functions,
+ each with one parameter. 
+The resulting function is calledcurried function
+*/
+
+const add = (a, b) => console.log(a + b)
+
+add(1, 2) // Result is 3
+
+const addCurried = a => b => console.log(a + b)
+
+addCurried(1)(2) // Result is also 3
+
+const anotherAddCurried = a => b => c => console.log(a + b + c)
+
+anotherAddCurried(1)(2)(3) // Result is 6
+
+/*
+CONDITIONAL RENDERING USING TRENARY
+//RULE OF THUMB, CANNOT USE if/else if, else, switch inside a render, can be used inside a function
+*/
+
+render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    return (
+        <div>
+            {isLoggedIn ? (
+                <LogoutButton onClick={this.handleLogoutClick} />
+            ) : (
+                    <LoginButton onClick={this.handleLoginClick} />
+                )}
+        </div>
+    );
+}
+
+/*
+handleChange & handleSubmit functions
+*/
+//hypothetically if there was a state object called value
+//we would modify it via a input field(form)
+//thus submitting this form would alert us of state object value, and prevent default behaviour of browser
+handleChange(event) {
+    this.setState({ value: event.target.value });
+}
+
+handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+}
+
+/*
+PROPTYPES GOOD FOR VALIDATION
+*/
+<h1>{this.props.name}</h1>
+
+Greeting.propTypes = {
+    name: PropTypes.string
+};
+// Specifies the default values for props:
+Greeting.defaultProps = {
+    name: 'Stranger'
+};
+
+//specifying type
+optionalArray: PropTypes.array,
+    optionalBool: PropTypes.bool,
+        optionalFunc: PropTypes.func,
+            optionalNumber: PropTypes.number,
+                optionalObject: PropTypes.object,
+                    optionalString: PropTypes.string,
+                        optionalSymbol: PropTypes.symbol,
+
+                        /*
+                        REQUIRING SINGLE CHILD, PASSING SINGLE CHILD TO A COMPONENT
+                        */
+    class MyComponent extends React.Component {
+        render() {
+            // This must be exactly one element or it will warn.
+            const children = this.props.children;
+            return (
+                <div>
+                    {children}
+                </div>
+            );
+        }
+    }
+
+MyComponent.propTypes = {
+    children: PropTypes.element.isRequired
+};
+
+/*
+FRAGMENTS
+component to return multiple elements
+group a list of children without adding extra nodes to the DOM
+*/
+//ALSO CAN BE ACHIEVED VIA <> </>, IDENTICAL
+render() {
+    return (
+        <React.Fragment>
+            <ChildA />
+            <ChildB />
+            <ChildC />
+        </React.Fragment>
+    );
+}
+
+
+
