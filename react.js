d8d959e4acf483f338d69082b12345f49688e41c
Tom <Thomas.Magin@Cognizant.com>
2019-05-03 12:43:01 +1000

    adding react errors

--- react.js
+++ react.js
@@ -49,3 +49,57 @@ render() {
 }
 
 export default StatefulComponent
+
+//functional component
+//Functional Components are functions, no state and lifecycle methods i.e stateless like above
+//easier to test, no state or side effects to worry about
+//easier to debug
+function Hello(props) {
+    return <div>Hello {props.name}</div>
+}
+//ES6 functional component, identical to one above
+const Hello = ({ name }) => <div>Hello {name}</div>
+
+//Functional components can reduce coupling 
+//One of the core concepts used to describe clean code is coupling.
+//Coupling describes the degree of dependency between one entity to another.
+//If our code has low coupling, 
+//it means that we can change one area of our code without impacting another. 
+
+
+//ERRORS
+
+
+/*
+ARRAY KEY MISSING WARNING
+*/
+
+const arr = ['red','blue','green'];
+
+//inside render and return
+<p>{arr.map(m=> <li>{m}</li>)}</p>
+//will produce warning key is required for arrays
+//fix <p key={m}>{m}</p>
+
+/*
+MUTATED A STATE VARIABLE
+*/
+
+state = {
+    arrow: ['violet','blossom'];
+}
+//function
+//bad - do not mutate object, unexpected behaviour
+changeColor=()=> {
+    const a = this.state.arrow;
+    a[0] = 'green';
+    this.setState({a});
+}
+//fix - spread ... or Object.assign
+//both maintain immutability of objects
+const a = [...this.state.arrow];
+const a = Object.assign({});
+
+/*
+setState is asynchronous
+*/
